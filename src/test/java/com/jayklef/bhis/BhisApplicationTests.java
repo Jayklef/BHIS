d1ed9e675c40ca164b66ae4385281532703d4df7
Jayklef <jerryarhawho@gmail.com>
2022-01-25 13:43:12 +0100

    Initial commit

---
+++ src/test/java/com/jayklef/bhis/BhisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayklef.bhis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
