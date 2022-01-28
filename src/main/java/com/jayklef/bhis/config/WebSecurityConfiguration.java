681ef02608d20db511dd31abc39603c79bcc5b39
Jayklef <jerryarhawho@gmail.com>
2022-01-28 10:37:35 +0100

    Congured roles and permissions

--- src/main/java/com/jayklef/bhis/config/WebSecurityConfiguration.java
+++ src/main/java/com/jayklef/bhis/config/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,4 +30,18 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return provider;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .antMatchers("/home")
+                .hasAnyAuthority("user")
+                .antMatchers("/books")
+                .hasAnyAuthority("admin")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
 }
